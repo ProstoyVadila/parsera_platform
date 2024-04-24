e403fe20cb7de81601b9856ca6889ab3bb0d77e9
Vadim Gorbachev <vadim.gorbachev.dev@gmail.com>
2024-04-24 22:56:59 +0800

    fmt + clippy

---
+++ scheduler/src/api.rs
@@ -0,0 +1,65 @@
+// #![allow(dead_code,unused)]
+use actix_web::{get, post, web, App, HttpServer};
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+use tokio_cron_scheduler::Job;
+use tracing_actix_web::{RequestId, TracingLogger};
+
+use crate::config::Config;
+use crate::SharedSheduler;
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+struct RoutineIn {
+    name: String,
+    rule: String,
+    task: String,
+}
+
+#[derive(Debug, Serialize)]
+struct RoutineOut {
+    info: RoutineIn,
+    routine_id: String,
+}
+
+#[post("/routine")]
+async fn add_routine(
+    routine: web::Json<RoutineIn>,
+    sched: web::Data<SharedSheduler>,
+    req_id: RequestId,
+) -> web::Json<RoutineOut> {
+    tracing::debug!("Adding a new routine for {}: {:?}", req_id, routine);
+
+    let rtn = routine.clone();
+    let job = Job::new(rtn.rule.as_str(), move |_uuid, _lock| {
+        tracing::warn!("{}: {}", rtn.name, rtn.task);
+    })
+    .unwrap_or_else(|_| panic!("cannot register a new job for request {}", req_id));
+
+    let job_id = sched.lock().unwrap().add(job).await.expect("cannot register a a new job");
+
+    web::Json(RoutineOut{
+        info: routine.0,
+        routine_id: job_id.to_string(),
+    })
+}
+
+#[get("/healthcheck")]
+async fn get_healthcheck() -> &'static str {
+    tracing::debug!("got healthcheck");
+    "ok"
+}
+
+pub async fn run_server(cfg: Config, sched: SharedSheduler) -> Result<()> {
+    tracing::info!("Starting web server on {}", cfg.get_socket_addr());
+    HttpServer::new(move || {
+        App::new()
+            .wrap(TracingLogger::default())
+            .app_data(web::Data::new(sched.clone()))
+            .service(add_routine)
+            .service(get_healthcheck)
+    })
+    .bind(cfg.get_socket_addr())?
+    .run()
+    .await?;
+    Ok(())
+}
--- scheduler/src/main.rs
+++ scheduler/src/main.rs
@@ -1,16 +1,14 @@
-#![allow(dead_code,unused)]
+#![allow(dead_code, unused)]
 use std::sync::{Arc, Mutex};
 
 use anyhow::{Ok, Result};
 use tokio_cron_scheduler::JobScheduler;
 
+mod api;
 mod config;
 mod jobs;
-mod api;
-
 
 pub type SharedSheduler = Arc<Mutex<JobScheduler>>;
-
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -18,12 +16,15 @@ async fn main() -> Result<()> {
     cfg.init_tracing();
 
     let sched = Arc::new(Mutex::new(JobScheduler::new().await?));
-    
-    jobs::register_initial_jobs(&mut sched.clone()).await?;
-    
-    tracing::info!("Starting scheduler...");
-    sched.lock().expect("cannot start scheduler").start().await?;
 
+    jobs::register_initial_jobs(&mut sched.clone()).await?;
+
+    tracing::info!("Starting scheduler...");
+    sched
+        .lock()
+        .expect("cannot start scheduler")
+        .start()
+        .await?;
 
     api::run_server(cfg, sched).await?;
     Ok(())
