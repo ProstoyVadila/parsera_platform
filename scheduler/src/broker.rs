42d1d75d8235a631151a0e223e4669933895e6fa
Vadim Gorbachev <vadim.gorbachev.dev@gmail.com>
2024-04-29 18:15:09 +0800

    improve error handling

--- scheduler/src/broker.rs
+++ scheduler/src/broker.rs
@@ -12,6 +12,8 @@ use deadpool_lapin::{Config, Manager, Pool, PoolConfig, Runtime};
 use rand::{rngs::ThreadRng, seq::SliceRandom};
 use tokio_stream::StreamExt;
 
+use common::{retry, increasing_retry};
+
 use crate::config::{BrokerConfig, DbAddr};
 use crate::{scheduler, SharedSheduler};
 use crate::utils::ParseraService;
@@ -50,14 +52,15 @@ impl Rabbit {
         Ok(rabbit)
     }
 
-    pub async fn get_channel(&self) -> Channel {
-        let conn = self.pool.get().await.expect("conn err");
-        let channel = conn.create_channel().await.expect("channel err");
-        channel
+    pub async fn get_channel(&self) -> Result<Channel> {
+        // TODO: remove expect (add result return)
+        let conn = retry!("get_rabbit_conn", self.pool.get().await, 10, 0.2)?;
+        let channel = retry!("get_rabbit_channel", conn.create_channel().await, 10, 0.2)?;
+        Ok(channel)
     }
 
     pub async fn declare_all(&self) {
-        let channel = self.get_channel().await;
+        let channel = self.get_channel().await.expect("cannot get a channel to declare");
         // Creating and binding produce exchange and queues
         self.declare_exchange(channel.clone(), &self.cfg.produce_exchange, ExchangeKind::Topic).await; 
         for queue in self.cfg.queues_to_produce() {
@@ -123,8 +126,8 @@ impl Rabbit {
         }
     }
 
-    pub async fn publish(&self, payload: &[u8], to: ParseraService) {
-        let channel = self.get_channel().await;
+    pub async fn publish(&self, payload: &[u8], to: ParseraService) -> Result<()> {
+        let channel = self.get_channel().await?;
         // let queue = self.choose_queue();
         // let pubOpts = BasicPublishOptions::default()
 
@@ -138,12 +141,14 @@ impl Rabbit {
             )
             .await
             .expect("cannot send message to the queue");
+        Ok(())
     }
 
     pub async fn consume(&self, sched: SharedSheduler) -> Result<()> {
         let queue = &self.cfg.queue_to_consume();
         tracing::info!("consuming from queue {}", queue);
-        let channel = self.get_channel().await;
+        let channel = self.get_channel().await.expect("cannot get a rabbit channel for consumer");
+
         let mut consumer = channel
             .basic_consume(
                 queue,
@@ -152,13 +157,10 @@ impl Rabbit {
                 FieldTable::default(),
             )
             .await?;
-        // TODO: fix error handling
+
         while let Some(delivery) = consumer.next().await {
             match delivery {
                 Ok(delivery) => {
-                    let event = str::from_utf8(&delivery.data)
-                        .expect("error converting message from consumer");
-                    tracing::debug!("Got event from consumer: {}", event);
                     scheduler::handle_event(self, sched.clone(), &delivery.data).await;
                     channel
                         .basic_ack(delivery.delivery_tag, BasicAckOptions::default())
@@ -166,7 +168,6 @@ impl Rabbit {
                 }
                 Err(err) => {
                     tracing::error!("Error caught in consumer: {}", err);
-                    break;
                 }
             }
         }
